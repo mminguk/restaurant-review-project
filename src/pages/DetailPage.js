ef7da7a6d798b02d90c0803aab9adf2883f4b1cf
Minkook <minguk2425@gmail.com>
2025-09-08 22:43:32 +0900

    decorate DetailPage

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,6 +1,6 @@
 import Footer from "../footer";
 import Nav from "../Nav";
-import Kakao from "../components/Kakao";
+
 
 function DetailPage(){
     return <>
@@ -9,12 +9,28 @@ function DetailPage(){
               <h1>가게상호명</h1>
         </div>
         <div id="article-middle">
-            <img src={`${process.env.PUBLIC_URL}/assets/matzip1.jpg`} alt="가게사진" />
-            <h3>가게상호명</h3>
-            <p>00시 00구 00동</p>
-            <p>tel.041-0000-0000</p>
+            <img src={`${process.env.PUBLIC_URL}/assets/matzip1.jpg`} alt="가게사진" width='300px' />
+            <div id='restaurant-menu'>
+                <span><h3>대표메뉴</h3></span>
+                <p>00메뉴 --- 10000원</p>
+                <p>00메뉴 --- 20000원</p>
+                <p>00메뉴 --- 30000원</p>
+            </div>
+            <div id='restaurant-info'>
+                <span><h3>가게상호명</h3></span>
+                <p>00시 00구 00동</p>
+                <p>tel.041-0000-0000</p>
+            </div>
+            <div id='restaurant-review'>
+                <span id="review-border"><h3>리뷰</h3></span>
+                <div id="review-container">
+                    <div className='review'>
+                        <h4>000</h4>
+                        <p>맛있습니다!! 또 오겠습니다</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <Kakao />
         <Footer />
     </>
 }
